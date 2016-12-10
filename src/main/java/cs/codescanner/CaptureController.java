14d1b661648101f83a0332566397ec892cc86575
Rene Dohan <dohan.rene@gmail.com>
2016-12-10 17:16:46 -0600

    Improvement: Capture Controller now set orientation to landscape on  start and change it back to previous so it can be used inside inview instead on new activity

--- src/main/java/cs/codescanner/CaptureController.java
+++ src/main/java/cs/codescanner/CaptureController.java
@@ -1,32 +1,46 @@
 package cs.codescanner;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.WindowManager.LayoutParams;
-import cs.android.viewbase.LayoutId;
+
 import cs.android.viewbase.CSViewController;
+import cs.android.viewbase.LayoutId;
 import cs.codescanner.scanner.CaptureMainController;
 
 public abstract class CaptureController extends CSViewController {
 
-	protected final CaptureMainController capture = new CaptureMainController(this) {
-		@Override protected void onDecodeDone() {
-			CaptureController.this.onDecodeDone();
-		};
+    protected final CaptureMainController capture = new CaptureMainController(this) {
+        @Override
+        protected void onDecodeDone() {
+            CaptureController.this.onDecodeDone();
+        }
 
-		@Override protected void onFrameworkBug() {
-		}
-	};
+        ;
 
-	public CaptureController(CSViewController parent, LayoutId layout) {
-		super(parent, layout);
-	}
+        @Override
+        protected void onFrameworkBug() {
+        }
+    };
+    private int _requestedOrientationBefore;
 
-	@Override protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		activity().getWindow().addFlags(LayoutParams.FLAG_KEEP_SCREEN_ON);
-	}
-	
-	protected void onDecodeDone() {
-	}
+    public CaptureController(CSViewController parent, LayoutId layout) {
+        super(parent, layout);
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity().getWindow().addFlags(LayoutParams.FLAG_KEEP_SCREEN_ON);
+        _requestedOrientationBefore = activity().getRequestedOrientation();
+        activity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    public void onDestroy() {
+        activity().setRequestedOrientation(_requestedOrientationBefore);
+        super.onDestroy();
+    }
+
+    protected void onDecodeDone() {
+    }
 
 }
